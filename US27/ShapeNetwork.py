56dc247bc6dd86a609b8f8b0efcb5837a657f051
acriver3 <acriver3@asu.edu>
2019-10-26 18:52:00 -0700

    Task 39 added reset functionality to return config to a default state. Will add various write functions in future user stories.

--- US27/ShapeNetwork.py
+++ US27/ShapeNetwork.py
@@ -1,5 +1,19 @@
 import json
 
+default = {
+    "JSON": {
+        "databaseDir": "database/", 
+        "defaultDir": "database/"
+    }, 
+    "MongoDB": {
+        "host": "127.0.0.1", 
+        "name": "tibit_pon_controller", 
+        "port": "27017"
+    }, 
+    "databaseType": "JSON", 
+    "interface": "enp0s8.4090"
+}
+
 with open("PonCntlInit.json", "rt") as file:
 	jsonData = json.load(file)
 
@@ -13,3 +27,7 @@ print("MongoDB Host port: " + str(jsonData["MongoDB"]["port"]) + "\n")
 
 print("current database type: " + str(jsonData["databaseType"]) + "\n")
 print("current interface: " + str(jsonData["interface"]) + "\n")
+
+print("Resetting PonCntlInit.json \n")
+with open("PonCntlInit.json", "w") as file:
+	json.dump(default, file)
